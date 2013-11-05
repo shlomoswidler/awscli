7692c783b5dc4cf8603341b1ce9e8fe8e73ba177
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-11-05 14:01:27 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'shlomo.swidler@orchestratus.com'
 license          'Apache 2.0'
 description      'Installs the AWS command line tools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 recipe "default", "Install AWS CLI tools"
 
@@ -19,3 +19,28 @@ attribute 'awscli/compile_time',
   :type => "string",
   :required => "optional",
   :recipes => [ "awscli::default" ]
+  
+attribute 'awscli/aws_access_key_id',
+  :display_name => "AWS Access Key Id",
+  :description => "The access key ID. Used to configure the awscli tool.",
+  :default => "nil",
+  :type => "string",
+  :required => "optional",
+  :recipes => [ "awscli::default" ]
+  
+attribute 'awscli/aws_access_key_id',
+  :display_name => "AWS Secret Access Key",
+  :description => "The secret access key. Used to configure the awscli tool.",
+  :default => "nil",
+  :type => "string",
+  :required => "optional",
+  :recipes => [ "awscli::default" ]
+
+attribute 'awscli/default_region',
+  :display_name => "Default AWS region",
+  :description => "The default AWS region. Used to configure the awscli tool.",
+  :default => "nil",
+  :type => "string",
+  :required => "optional",
+  :recipes => [ "awscli::default" ]
+  
