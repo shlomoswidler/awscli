bf00b20806f03d70d224e427a0d2c66af1f7d6c6
Ron Lipke <ron@ronlipke.com>
2015-09-01 15:47:34 -0400

    Use `easy_install pip` for centos and redhat

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,9 +5,12 @@ when 'debian', 'ubuntu'
   file = '/usr/local/bin/aws'
   cmd = 'apt-get install -y python-pip && pip install awscli'
   completion_file = '/etc/bash_completion.d/aws'
-when 'redhat', 'centos', 'fedora', 'amazon', 'scientific'
+when 'fedora', 'amazon', 'scientific'
   file = '/usr/bin/aws'
   cmd = 'yum -y install python-pip && pip install awscli'
+when 'redhat', 'centos'
+  file = '/usr/bin/aws'
+  cmd = 'easy_install pip && pip install awscli'
 end
 r = execute 'install awscli' do
   command cmd
